f7d633f6842db58d002f2c6ca04f0d0b6f814249
Dikocesrt <dikocesrt@gmail.com>
2025-05-05 23:45:48 +0700

    added comment controllers

---
+++ controllers/comment.go
@@ -0,0 +1,45 @@
+package controllers
+
+import (
+	"makanan-app/entities"
+	"makanan-app/utils"
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+)
+
+type CommentController struct {
+	commentUsecase entities.CommentUsecaseInterface
+}
+
+func NewCommentController(commentUsecase entities.CommentUsecaseInterface) CommentController {
+	return CommentController{
+		commentUsecase: commentUsecase,
+	}
+}
+
+type commentRequest struct {
+	Name    string `json:"name"`
+	Comment string `json:"comment"`
+	NewsID  uint   `json:"news_id"`
+}
+
+func (c CommentController) CreateComment(ctx echo.Context) error {
+	comment := commentRequest{}
+	if err := ctx.Bind(&comment); err != nil {
+		return err
+	}
+
+	_, err := c.commentUsecase.CreateComment(entities.Comment{
+		Name:    comment.Name,
+		Comment: comment.Comment,
+		News: entities.News{
+			ID: comment.NewsID,
+		},
+	})
+	if err != nil {
+		return ctx.JSON(utils.ConvertErrorCode(err), utils.NewBaseErrorResponse(err.Error()))
+	}
+
+	return ctx.JSON(http.StatusCreated, utils.NewBaseSuccessResponse("success create comment", struct{}{}))
+}
